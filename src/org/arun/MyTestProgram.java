3dd21a06e34c158a5d6f374630e0ca7847711329
ARUN <ARUN@LAPTOP-DST4NNU8>
2018-08-11 21:05:18 +0530

    Initial Commit.

---
+++ src/org/arun/MyTestProgram.java
@@ -0,0 +1,11 @@
+package org.arun;
+
+public class MyTestProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hi! Welcome Arun!!!");
+		
+		System.out.println("I am a Software Engineer.");
+	}
+
+}
